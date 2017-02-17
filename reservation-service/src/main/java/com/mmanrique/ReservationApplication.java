6159484ea6912a7d212537e10199c16e8556d671
mmanrique <memo881@gmail.com>
2017-02-17 00:26:36 -0600

    Adding the EurekaServer config

--- reservation-service/src/main/java/com/mmanrique/ReservationApplication.java
+++ reservation-service/src/main/java/com/mmanrique/ReservationApplication.java
@@ -3,11 +3,13 @@ package com.mmanrique;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReservationApplication {
 
     public static void main(String[] args) {
