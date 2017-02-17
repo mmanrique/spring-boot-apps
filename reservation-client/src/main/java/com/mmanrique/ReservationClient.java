a03882edfa85c7cf8de7804d4b6ec905a9ffd72f
mmanrique <memo881@gmail.com>
2017-02-17 00:55:31 -0600

    Zuul proxy to internal services

---
+++ reservation-client/src/main/java/com/mmanrique/ReservationClient.java
@@ -0,0 +1,16 @@
+package com.mmanrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZuulProxy
+public class ReservationClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationClient.class);
+    }
+}
